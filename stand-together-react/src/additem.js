9f4b15dc81915217358b82b00a8d2628633b3935
Michelle Bergquist <caker25@gmail.com>
2017-07-07 15:24:37 -0600

    Add comments for next features, form & data

--- stand-together-react/src/additem.js
+++ stand-together-react/src/additem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// import { Field, reduxForm } from 'redux-form'
 
 // HEY HERE
 
@@ -7,8 +8,6 @@ const AddItem = (props) => {
   return (
     <div>
       <section class='linkform'>
-        <p>from additem.js</p>
-        <p>your link is {title}</p>
         <label for='title'>Title</label>
         <input type='text' class='' name='title' id='title' />
         <label for='name'>Name</label>
@@ -20,5 +19,69 @@ const AddItem = (props) => {
     </div>
   )
 }
+
+// const AddItem = (props) => {
+//   const { handleSubmit, pristine, reset, submitting } = props
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div>
+//         <label>First Name</label>
+//         <div>
+//           <Field name='firstName' component='input' type='text' placeholder='First Name' />
+//         </div>
+//       </div>
+//       <div>
+//         <label>Last Name</label>
+//         <div>
+//           <Field name='lastName' component='input' type='text' placeholder='Last Name' />
+//         </div>
+//       </div>
+//       <div>
+//         <label>Email</label>
+//         <div>
+//           <Field name='email' component='input' type='email' placeholder='Email' />
+//         </div>
+//       </div>
+//       <div>
+//         <label>Sex</label>
+//         <div>
+//           <label><Field name='sex' component='input' type='radio' value='male' /> Male</label>
+//           <label><Field name='sex' component='input' type='radio' value='female' /> Female</label>
+//         </div>
+//       </div>
+//       <div>
+//         <label>Favorite Color</label>
+//         <div>
+//           <Field name='favoriteColor' component='select'>
+//             <option />
+//             <option value='ff0000'>Red</option>
+//             <option value='00ff00'>Green</option>
+//             <option value='0000ff'>Blue</option>
+//           </Field>
+//         </div>
+//       </div>
+//       <div>
+//         <label htmlFor='employed'>Employed</label>
+//         <div>
+//           <Field name='employed' id='employed' component='input' type='checkbox' />
+//         </div>
+//       </div>
+//       <div>
+//         <label>Notes</label>
+//         <div>
+//           <Field name='notes' component='textarea' />
+//         </div>
+//       </div>
+//       <div>
+//         <button type='submit' disabled={pristine || submitting}>Submit</button>
+//         <button type='button' disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+//       </div>
+//     </form>
+//   )
+// }
+//
+// export default reduxForm({
+//   form: 'simple'  // a unique identifier for this form
+// })(AddItem)
 
 export default AddItem
--- stand-together-react/src/topicitem.js
+++ stand-together-react/src/topicitem.js
@@ -3,19 +3,23 @@ import AddItem from './additem'
 import styles from './splash.css'
 
 // HEY HERE
+// const data = {
+//   discussionItems: [
+//     { title: 'Develop Denver Happy Hour', name: 'Michelle', details: 'Tonight, 6pm at a bar.'},
+//     { title: 'Develop Denver Happy Hour', name: 'Michelle', details: 'Tonight, 6pm at a bar.'}
+//   ]
+// }
 
 const TopicItem = (props) => {
   const {title} = props
-
   return (
     <div className='TopicBox'>
       <div className='parentFlex'>
         <h1 className='itemHeader'>{title}</h1>
-        <h1 className='right'>+</h1>
+        <h1 className='right addStuff'>+</h1>
       </div>
       {/* make hidden */}
       <AddItem />
-      {/* here */}
       <hr />
       <div>insert discussion items here</div>
       <ul>
