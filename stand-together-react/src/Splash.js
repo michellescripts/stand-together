b15ff75e995ec98325c3e6fc1dc7baf0da770a18
Michelle Bergquist <caker25@gmail.com>
2017-09-24 13:07:13 -0600

    Add Route

--- stand-together-react/src/Splash.js
+++ stand-together-react/src/Splash.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import FontAwesome from 'react-fontawesome'
+import { withRouter } from 'react-router-dom'
 
 class NewTeam extends React.Component {
   constructor () {
@@ -20,38 +21,40 @@ class NewTeam extends React.Component {
       method: 'POST'
     }).then(response => {
       if (response.ok) {
-        // add route to bp for team
+        const todaysDate = new Date().toISOString().slice(0, 10).replace(/-/g, '')
+        const urlPath = `/team/${this.state.teamName}/${todaysDate}`
+        this.props.history.push(urlPath)   
       } else {
         this.setState({errorMode: true})
       }
     })
-    
   }
   render () {
     let styleClasses = 'right button'
     if (this.state.errorMode) {styleClasses += ' errorMode'}
-    if (this.state.showButton === true) {
-      return (
-        <button className='right button' onClick={this.changeElement}><FontAwesome className='fa' name='plus' />Create a Team</button>
-      )
-    } else {
-      return (
-        <div>
-          <input className={styleClasses} onChange={this.handleTeam} ></input>
-          <button className='right button' onClick={this.addTeam}><FontAwesome className='fa' name='plus' /></button>
-          <button className='right button' onClick={this.cancel}><FontAwesome className='fa' name='plus' /></button>
+    return (
+      <div>
+        {this.state.showButton 
+          ?   <button className='right button' onClick={this.changeElement}><FontAwesome className='fa' name='plus' />Create a Team</button> 
+          :   <div>
+                <input className={styleClasses} onChange={this.handleTeam} ></input>
+                <button className='right button' onClick={this.addTeam}><FontAwesome className='fa' name='plus' /></button>
+                <button className='right button' onClick={this.cancel}><FontAwesome className='fa' name='plus' /></button>
+              </div>
+        }
       </div>
-      )
-    }
+    )
   }
 }
+
+const NewTeamWithRouter = withRouter(NewTeam)
 
 const Splash = () => (
   <div>
     <div className='homeNavBar'>
       <h2 className='title'>Stand Together<FontAwesome name='users' /></h2>
       <button className='right button'><a href='/new'><FontAwesome className='fa' name='plus' />Create a Stand Up</a></button>
-      <NewTeam />
+      <NewTeamWithRouter/>
     </div>
     <div className='splashImage' />
   </div>
